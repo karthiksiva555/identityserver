3b4e7810f0055bef74e486bc752a699acbacf4a3
Siva Karthik Narisetty <karthiksmac@Sivas-MBP.ht.home>
2022-09-27 16:54:11 -0400

    First commit - working ids, api and client

---
+++ identityclient/Program.cs
@@ -0,0 +1,50 @@
+﻿using IdentityModel.Client;
+using System.Text.Json;
+
+HttpClientHandler clientHandler = new HttpClientHandler();
+clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+
+// Pass the handler to httpclient(from you are calling api)
+var client = new HttpClient(clientHandler);
+
+var disco = await client.GetDiscoveryDocumentAsync("https://localhost:5001");
+if (disco.IsError)
+{
+    Console.WriteLine(disco.Error);
+    return;
+}
+
+// request token
+var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+{
+    Address = disco.TokenEndpoint,
+    ClientId = "client",
+    ClientSecret = "secret",
+
+    Scope = "api1"
+});
+
+if (tokenResponse.IsError)
+{
+    Console.WriteLine(tokenResponse.Error);
+    Console.WriteLine(tokenResponse.ErrorDescription);
+    return;
+}
+
+Console.WriteLine(tokenResponse.Json);
+Console.WriteLine("\n\n");
+
+// call api
+var apiClient = new HttpClient(clientHandler);
+apiClient.SetBearerToken(tokenResponse.AccessToken);
+
+var response = await apiClient.GetAsync("https://localhost:5011/identity");
+if (!response.IsSuccessStatusCode)
+{
+    Console.WriteLine(response.StatusCode);
+}
+else
+{
+    var doc = JsonDocument.Parse(await response.Content.ReadAsStringAsync()).RootElement;
+    Console.WriteLine(JsonSerializer.Serialize(doc, new JsonSerializerOptions { WriteIndented = true }));
+}
---
+++ sampleapi/Controllers/IdentityController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Api.Controllers;
+
+[ApiController]
+[Authorize]
+[Route("identity")]
+public class IdentityController: ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return new JsonResult(from c in User.Claims select new { c.Type, c.Value });
+    }
+}
